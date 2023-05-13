eedabf7c1225f39e1006c5b59dc09837f334f246
Andrea Diaz Correia <andrea.diaz.correia@gmail.com>
2023-05-12 23:48:54 -0300

    Keep calm and commit

---
+++ src/test/page.test.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import Home from '../app/page';
+
+describe('Home Page', () => {
+  it('renders without errors', () => {
+    const spy = jest.spyOn(console, 'error');
+    render(<Home />);
+    expect(spy).not.toHaveBeenCalled();
+  });
+});
+
+
+
+
+
+
